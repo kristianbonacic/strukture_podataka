f7f826ff5cfd0929a22bc96c6c799dc2cc027407
Kristian Bonačić <kristianbonacic6@gmail.com>
2025-10-29 20:28:16 +0100

    Update Vjezba3doubly.c

--- Vjezba3doubly.c
+++ Vjezba3doubly.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,37 +38,69 @@ Student* addEnd(Student* head, Student* newStudent) {
     return head;
 }
 
-Student* addAfter(Student* target, Student* newStudent) {
-    newStudent->next = target->next;
-    newStudent->prev = target;
-    if (target->next != NULL) target->next->prev = newStudent;
-    target->next = newStudent;
-    return newStudent;
+Student* findBySurname(Student* head, char surname[]) {
+    Student* temp = head;
+    while (temp != NULL) {
+        if (strcmp(temp->surname, surname) == 0) return temp;
+        temp = temp->next;
+    }
+    return NULL;
 }
 
-Student* addBefore(Student** head, Student* target, Student* newStudent) {
-    newStudent->next = target;
-    newStudent->prev = target->prev;
-    if (target->prev != NULL) target->prev->next = newStudent;
-    else *head = newStudent;
-    target->prev = newStudent;
-    return newStudent;
+Student* deleteNode(Student** head, Student* target) {
+    if (!target) return *head;
+    if (target->prev) target->prev->next = target->next;
+    else *head = target->next;
+    if (target->next) target->next->prev = target->prev;
+    free(target);
+    return *head;
 }
 
 Student* printList(Student* head) {
     Student* temp = head;
-    while (temp != NULL) {
+    while (temp) {
         printf("%s %s (%d)\n", temp->name, temp->surname, temp->age);
         temp = temp->next;
     }
     return head;
 }
 
-Student* deleteNode(Student** head, Student* target) {
-    if (!target) return *head;
-    if (target->prev != NULL) target->prev->next = target->next;
-    else *head = target->next;
-    if (target->next != NULL) target->next->prev = target->prev;
-    free(target);
-    return *head;
+int main() {
+    Student* head = NULL;
+    int choice, age;
+    char name[50], surname[50];
+
+    while (1) {
+        printf("\n--- MENU ---\n");
+        printf("1 - Add at beginning\n2 - Add at end\n3 - Delete by surname\n4 - Print\n0 - Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter name, surname, age: ");
+                scanf("%s %s %d", name, surname, &age);
+                head = addBeginning(head, createStudent(name, surname, age));
+                break;
+            case 2:
+                printf("Enter name, surname, age: ");
+                scanf("%s %s %d", name, surname, &age);
+                head = addEnd(head, createStudent(name, surname, age));
+                break;
+            case 3: {
+                printf("Enter surname to delete: ");
+                scanf("%s", surname);
+                Student* target = findBySurname(head, surname);
+                head = deleteNode(&head, target);
+                break;
+            }
+            case 4:
+                head = printList(head);
+                break;
+            case 0:
+                return 0;
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
 }
